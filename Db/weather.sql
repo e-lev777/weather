cf95870c020845e0e0ef6261e1888e70d1f988a7
Yevheniy <evheniy.lykholay@gmail.com>
2015-12-01 12:06:01 +0200

    Merge remote-tracking branch 'remotes/origin/master'
    # Conflicts:
    #	View/Articles/articles.phtml

--- Db/weather.sql
+++ Db/weather.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: localhost
--- Время создания: Дек 01 2015 г., 11:46
+-- Время создания: Дек 01 2015 г., 12:03
 -- Версия сервера: 5.6.25-log
 -- Версия PHP: 5.6.15
 
@@ -73,60 +73,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `active` tinyint(1) NOT NULL,
   `access` tinyint(4) NOT NULL DEFAULT '0',
   `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `users`
 --
 
 INSERT INTO `users` (`id`, `login`, `hash_password`, `email`, `active`, `access`, `reg_date`) VALUES
-(9, 'evheniy', '0be612d4eba83a933240207ed7110a93', 'evheniy.lykholay@gmail.com', 1, 0, '2015-10-14 08:55:56'),
-(10, 'Bohdan', '0be612d4eba83a933240207ed7110a93', 'rozboh@gmail.com', 1, 5, '2015-10-14 08:55:56');
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `weather_data`
---
-
-CREATE TABLE IF NOT EXISTS `weather_data` (
-  `id` int(10) unsigned NOT NULL,
-  `date` date NOT NULL,
-  `city_id` int(10) unsigned NOT NULL,
-  `city_name` varchar(70) NOT NULL,
-  `morning_temperature` varchar(3) DEFAULT NULL,
-  `morning_temperature_from` varchar(3) DEFAULT NULL,
-  `morning_temperature_to` varchar(3) DEFAULT NULL,
-  `morning_image` varchar(255) NOT NULL,
-  `day_temperature` varchar(3) DEFAULT NULL,
-  `day_temperature_from` varchar(3) DEFAULT NULL,
-  `day_temperature_to` varchar(3) DEFAULT NULL,
-  `day_image` varchar(255) NOT NULL,
-  `evening_temperature` varchar(3) DEFAULT NULL,
-  `evening_temperature_from` varchar(3) DEFAULT NULL,
-  `evening_temperature_to` varchar(3) DEFAULT NULL,
-  `evening_image` varchar(255) NOT NULL,
-  `night_temperature` varchar(3) DEFAULT NULL,
-  `night_temperature_from` varchar(3) DEFAULT NULL,
-  `night_temperature_to` varchar(3) DEFAULT NULL,
-  `night_image` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
-
---
--- Дамп данных таблицы `weather_data`
---
-
-INSERT INTO `weather_data` (`id`, `date`, `city_id`, `city_name`, `morning_temperature`, `morning_temperature_from`, `morning_temperature_to`, `morning_image`, `day_temperature`, `day_temperature_from`, `day_temperature_to`, `day_image`, `evening_temperature`, `evening_temperature_from`, `evening_temperature_to`, `evening_image`, `night_temperature`, `night_temperature_from`, `night_temperature_to`, `night_image`) VALUES
-(1, '2015-11-27', 33487, 'Черкассы', NULL, '-1', '0', 'ovc', NULL, '0', '+1', 'ovc', NULL, '-1', '0', 'ovc', '-2', NULL, NULL, 'ovc'),
-(2, '2015-11-28', 33487, 'Черкассы', NULL, '-2', '+1', 'bkn_d', NULL, '+1', '+2', 'ovc', '+2', NULL, NULL, 'ovc', '+2', NULL, NULL, 'ovc_ra'),
-(3, '2015-11-29', 33487, 'Черкассы', NULL, '+2', '+3', 'ovc_+ra', NULL, '+3', '+4', 'ovc_ra', NULL, '+3', '+5', 'ovc_-ra', NULL, '+1', '+3', 'ovc_-ra'),
-(4, '2015-11-30', 33487, 'Черкассы', NULL, '0', '+1', 'bkn_d', NULL, '0', '+3', 'ovc', NULL, '+3', '+4', 'ovc_ra', NULL, '+2', '+4', 'ovc_-ra'),
-(5, '2015-12-01', 33487, 'Черкассы', NULL, '+2', '+4', 'bkn_d', NULL, '+4', '+7', 'ovc', NULL, '+5', '+7', 'ovc_ra', NULL, '+2', '+5', 'ovc'),
-(6, '2015-12-02', 33487, 'Черкассы', NULL, '+1', '+2', 'ovc', NULL, '+1', '+3', 'ovc', NULL, '+2', '+3', 'ovc', NULL, '+1', '+2', 'ovc'),
-(7, '2015-12-03', 33487, 'Черкассы', NULL, '+1', '+2', 'ovc', NULL, '+2', '+6', 'ovc', NULL, '+5', '+6', 'ovc', NULL, '+3', '+5', 'ovc'),
-(8, '2015-12-04', 33487, 'Черкассы', NULL, '+2', '+3', 'ovc', NULL, '+3', '+6', 'ovc', NULL, '+4', '+6', 'ovc', NULL, '+1', '+4', 'ovc'),
-(9, '2015-12-05', 33487, 'Черкассы', '+1', NULL, NULL, 'ovc', NULL, '+1', '+4', 'ovc', NULL, '+2', '+4', 'ovc', NULL, '0', '+2', 'ovc'),
-(10, '2015-12-06', 33487, 'Черкассы', '0', NULL, NULL, 'ovc', NULL, '0', '+3', 'ovc', NULL, '+1', '+3', 'ovc', NULL, '0', '+1', 'ovc');
+(11, 'admin', '462d8f9b46b783f4d4693f733e2ee11f', 'admin@gmail.com.ua', 1, 5, '2015-12-01 09:59:02');
 
 -- --------------------------------------------------------
 
@@ -169,12 +123,6 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- Индексы таблицы `weather_data`
---
-ALTER TABLE `weather_data`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Индексы таблицы `yandex_default_values`
 --
 ALTER TABLE `yandex_default_values`
@@ -198,12 +146,7 @@ ALTER TABLE `sources`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
---
--- AUTO_INCREMENT для таблицы `weather_data`
---
-ALTER TABLE `weather_data`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT для таблицы `yandex_default_values`
 --
